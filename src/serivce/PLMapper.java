eb94eeffd449b0a19666327dfc2f2032c9d0ee61
Hjaehong <zero5140@naver.com>
2022-11-13 21:49:32 +0900

    기본적인 임포

--- src/serivce/PLMapper.java
+++ src/serivce/PLMapper.java
@@ -3,7 +3,11 @@
   */
 package serivce;
 
-import org.apache.ibatis.annotations.Result;
+import java.util.ArrayList;
+
+import org.apache.ibatis.annotations.Select;
+
+import pl.model.dto.PLUserDTO;
 
 /**
   * @FileName : PLMapper.java
@@ -14,6 +18,16 @@ import org.apache.ibatis.annotations.Result;
   * @프로그램 설명 :
   */
 public interface PLMapper {
-	
-	
+
+	/**
+	  * @Method Name : selectAllPlace
+	  * @작성일 : 2022. 11. 11.
+	  * @작성자 : heojaehong
+	  * @변경이력 : 
+	  * @Method 설명 : 내저장소에 있는 장소를 다 보여준다.
+	  * @return
+	  */
+	@Select("SELECT NAME FROM NEWVELOPER")
+	ArrayList<PLUserDTO> selectAllPlace();
+
 }
